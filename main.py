2ab822b29a746bfed3a499ea4b360e8662d2791a
cmooiman <cmooiman@outlook.com>
2025-02-12 16:24:12 +0100

    added character count functionality

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def main(book_path):
     text = get_text(book_path)
     word_count = get_word_count(text)
-    print(f"The file '{book_path}' has {word_count} words.")
+    occurence = get_character_counts(text)
+    print(f"The file '{book_path}' contains {word_count} words.")
+    for key, value in occurence.items():
+        if key == '\n':
+            print(f"Character '\\n' appears '{value}' times.")            
+        else:
+            print(f"Character '{key}' appears '{value}' times.")
 
 def get_text(book_path):
     with open(book_path) as f:
@@ -10,5 +16,17 @@ def get_text(book_path):
 def get_word_count(text):
     word_count = text.split()
     return len(word_count)
+
+def get_character_counts(text):
+    lowered_text = text.lower()
+    occurence = {}
+    for i in lowered_text:
+        if i in occurence:
+            occurence[i] += 1
+        else:
+            occurence[i] = 1
+    return occurence
+
+
 
 main("books/frankenstein.txt")
